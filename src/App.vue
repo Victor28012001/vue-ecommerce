150b8433950d6ea319af2a2b0bbfad32697217e3
Victor <ifeanyivictor615@gmail.com>
2025-05-14 15:39:51 +0100

    banner added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="font-sans text-gray-900">
+    <Banner />
     <Header />
     <router-view />
     <Footer />
@@ -7,6 +8,7 @@
 </template>
 
 <script setup>
+import Banner from './components/Banner.vue';
 import Header from './components/Header.vue'
 import Footer from './components/Footer.vue'
 </script>
---
+++ src/components/Banner.vue
@@ -0,0 +1,41 @@
+<template>
+    <section class="hero">
+      <div class="new-arrivals">
+        <button class="new-tag">New</button>
+        <span class="sale-text">Sale ₦1200 Off Your First Order</span>
+      </div>
+    </section>
+  </template>
+  
+  <style scoped>
+  .hero {
+    background-color: #224893;
+    padding: 1rem;
+    display: flex;
+    justify-content: flex-end;
+  }
+  
+  .new-arrivals {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem; /* little gap between items */
+    color: white;
+  }
+  
+  .new-tag {
+    background: transparent;
+    border: 1px solid white;
+    color: white;
+    padding: 0.3rem 0.8rem;
+    border-radius: 9999px; /* capsule shape */
+    font-size: 0.875rem;
+    font-weight: 600;
+    cursor: default;
+  }
+  
+  .sale-text {
+    font-size: 0.875rem;
+    font-weight: 400;
+  }
+  </style>
+  
